f9c3735779d840b0c03f143c8d173b8892e6f024
Jose Mendoza <mendozaj9486@gmail.com>
2022-06-14 14:11:59 -0500

    Created interfaces for business and data layers

--- DataLayer/IRepository.cs
+++ DataLayer/IRepository.cs
@@ -1,5 +1,14 @@
-﻿namespace DataLayer;
+﻿using Models;
+
+namespace DataLayer;
 public interface IRepository
 {
-
+    Task<List<Question>> GetQuestionsAsync(string category);
+    Task<List<Answer>> GetAnswersAsync(int question_id);
+    Task<int> CreateTeamAsync(Team team);
+    Task UpdateTeamAsync(Team team);
+    Task CreatePlayerAsync(Player player);
+    Task<List<Team>> GetTeamsSortedbyScoreAsync();
+    Task<List<Player>> GetTeamMembersAsync(int team_id);
+    Task<List<Models.Type>> GetTypesAsync();
 }
