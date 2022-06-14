9f78dda52f5a082a3e1c535d5d044fb94159c424
Jose Mendoza <mendozaj9486@gmail.com>
2022-06-14 14:45:57 -0500

    Created question controller

---
+++ JeopardyAPI/Controllers/QuestionController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using BusinessLayer;
+
+namespace JeopardyAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class QuestionController : ControllerBase
+{
+    private readonly IBusiness _bl;
+    private readonly ILogger<QuestionController> _logger;
+
+    public QuestionController(IBusiness bl, ILogger<QuestionController> logger)
+    {
+        _bl = bl;
+        _logger = logger;
+    }
+
+    [HttpGet("GetQuestions/{category}")]
+    public async Task<ActionResult<List<Question>>> Get(int category)
+    {
+        List<Question> questions = await _bl.GetQuestionsAsync(category);
+        if (questions != null)
+        {
+            return Ok(questions);
+        }
+        return NoContent();
+    }
+}
