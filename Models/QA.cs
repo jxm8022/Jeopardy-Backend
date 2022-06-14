c5221326b7f6363ed03b62620b9b97d2efac3892
Jose Mendoza <mendozaj9486@gmail.com>
2022-06-14 14:52:09 -0500

    Added quesiton and answer model combined

---
+++ Models/QA.cs
@@ -0,0 +1,7 @@
+namespace Models;
+
+public class QA
+{
+    public List<Question> Questions { get; set; } = new List<Question>();
+    public List<Answer> Answers { get; set; } = new List<Answer>();
+}
