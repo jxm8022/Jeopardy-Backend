9fb3c505c7b98196ecfd529d48368ac5edbb48f1
Jose Mendoza <mendozaj9486@gmail.com>
2022-06-14 14:12:37 -0500

    Added base code to Business and DBRepository

--- BusinessLayer/Business.cs
+++ BusinessLayer/Business.cs
@@ -1,6 +1,47 @@
+using DataLayer;
+using Models;
+
 namespace BusinessLayer;
 
 public class Business : IBusiness
 {
+    private readonly IRepository _repo;
 
+    public Business(IRepository repo)
+    {
+        _repo = repo;
+    }
+
+    public async Task<List<Question>> GetQuestionsAsync(string category)
+    {
+        return await _repo.GetQuestionsAsync(category);
+    }
+    public async Task<List<Answer>> GetAnswersAsync(int question_id)
+    {
+        return await _repo.GetAnswersAsync(question_id);
+    }
+    public async Task<int> CreateTeamAsync(Team team)
+    {
+        return await _repo.CreateTeamAsync(team);
+    }
+    public async Task UpdateTeamAsync(Team team)
+    {
+        await _repo.UpdateTeamAsync(team);
+    }
+    public async Task CreatePlayerAsync(Player player)
+    {
+        await _repo.CreatePlayerAsync(player);
+    }
+    public async Task<List<Team>> GetTeamsSortedbyScoreAsync()
+    {
+        return await _repo.GetTeamsSortedbyScoreAsync();
+    }
+    public async Task<List<Player>> GetTeamMembersAsync(int team_id)
+    {
+        return await _repo.GetTeamMembersAsync(team_id);
+    }
+    public async Task<List<Models.Type>> GetTypesAsync()
+    {
+        return await _repo.GetTypesAsync();
+    }
 }
--- DataLayer/DBRepository.cs
+++ DataLayer/DBRepository.cs
@@ -1,4 +1,5 @@
 using System.Data.SQLite;
+using Models;
 
 namespace DataLayer;
 
@@ -9,5 +10,38 @@ public class DBRepository : IRepository
     public DBRepository(string connectionString)
     {
         _connectionString = connectionString;
+    }
+
+    public async Task<List<Question>> GetQuestionsAsync(string category)
+    {
+        return new List<Question>();
+    }
+    public async Task<List<Answer>> GetAnswersAsync(int question_id)
+    {
+        return new List<Answer>();
+    }
+    public async Task<int> CreateTeamAsync(Team team)
+    {
+        return -1;
+    }
+    public async Task UpdateTeamAsync(Team team)
+    {
+
+    }
+    public async Task CreatePlayerAsync(Player player)
+    {
+
+    }
+    public async Task<List<Team>> GetTeamsSortedbyScoreAsync()
+    {
+        return new List<Team>();
+    }
+    public async Task<List<Player>> GetTeamMembersAsync(int team_id)
+    {
+        return new List<Player>();
+    }
+    public async Task<List<Models.Type>> GetTypesAsync()
+    {
+        return new List<Models.Type>();
     }
 }
