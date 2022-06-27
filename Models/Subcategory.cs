4f2d094a269620d8911d1b8e0cf4648769df79b6
Jose Mendoza <mendozaj9486@gmail.com>
2022-06-27 15:32:49 -0500

    Editing models to seperate categories and subcategories

---
+++ Models/Subcategory.cs
@@ -0,0 +1,8 @@
+namespace Models;
+
+public class Subcategory
+{
+    public int subcategory_id { get; set; } = -1;
+    public string subcategory_name { get; set; } = "";
+    public int category_id { get; set; } = -1;
+}
